bdfdb9afda583303903a2262edfd863420aa068b
Henry Stanley <henry@henrystanley.com>
2014-11-10 11:35:52 +0000

    Added main class

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,3 +1,12 @@
-public static void main (String[] args) {
+public class FizzBuzz {
+
+  public static void main (String[] args) {
+    // the 'main' class is the one that runs first
+    // when the program is run. Your program needs
+    // one of them.
+
+    System.out.print("You rule!");
+
+  }
 
 }
